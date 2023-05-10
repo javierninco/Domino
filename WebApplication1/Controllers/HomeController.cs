da497cb8f63d1b751c1c39321af63556a68a2dc2
ninvoltz <hernandonincodiaz@gmail.com>
2023-05-09 21:18:47 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Ficha
+    {
+        public int Izquierda { get; set; }
+        public int Derecha { get; set; }
+    }
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class DominoController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Ordenar([FromBody] List<Ficha> fichas)
+        {
+            if (fichas.Count < 2 || fichas.Count > 6)
+            {
+                return BadRequest("El conjunto de fichas debe tener entre 2 y 6 fichas.");
+            }
+
+
+
+            var fichasOrdenadas = new List<Ficha> { fichas.First() };
+            fichas.RemoveAt(0);
+
+            while (fichas.Any())
+            {
+                var ultimaFicha = fichasOrdenadas.Last();
+                var siguienteFicha = fichas.FirstOrDefault(f =>
+                    f.Izquierda == ultimaFicha.Derecha || f.Derecha == ultimaFicha.Derecha);
+
+                if (siguienteFicha == null)
+                {
+                    return BadRequest("No se pudo construir una cadena correcta de fichas.");
+                }
+
+                if (siguienteFicha.Izquierda == ultimaFicha.Derecha)
+                {
+                    fichasOrdenadas.Add(siguienteFicha);
+                }
+                else
+                {
+                    fichasOrdenadas.Add(new Ficha { Izquierda = siguienteFicha.Derecha, Derecha = siguienteFicha.Izquierda });
+                }
+
+                fichas.Remove(siguienteFicha);
+            }
+            if (fichasOrdenadas.First().Izquierda != fichasOrdenadas.Last().Derecha)
+            {
+                return BadRequest("Los números primero y último no son los mismos.");
+            }
+            return Ok(fichasOrdenadas);
+        }
+
+    }
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/Autenticacion.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
+
+namespace WebApplication1.Models
+{
+    //clase encargada de realizar la valida
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private const string AuthorizationHeaderName = "Authorization";
+        private const string BasicSchemeName = "Basic";
+
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey(AuthorizationHeaderName))
+            {
+                return AuthenticateResult.Fail("Missing authorization header");
+            }
+
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers[AuthorizationHeaderName]);
+
+            if (authHeader.Scheme != BasicSchemeName)
+            {
+                return AuthenticateResult.Fail("Invalid authorization scheme");
+            }
+
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+            var username = credentials[0];
+            var password = credentials[1];
+
+            // Verificar las credenciales del usuario
+            if (!IsValidUser(username, password))
+            {
+                return AuthenticateResult.Fail("Invalid username or password");
+            }
+
+            // Crear una identidad del usuario y establecer las propiedades de autenticación
+            var claims = new[] { new Claim(ClaimTypes.Name, username) };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+
+        private bool IsValidUser(string username, string password)
+        {
+            // Verificar las credenciales del usuario con alguna lógica de negocio
+            return (username == "Prueba" && password == "Pruebas123@");
+        }
+    }
+
+}
